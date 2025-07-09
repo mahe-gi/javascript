c770c4fcb99a5a5ac92108d3f78f087c1bd273cd
mahesh ch <chmahesh.dev@gmail.com>
2025-07-10 00:55:57 +0530

    assignments

---
+++ practice/dom/day1/script.js
@@ -0,0 +1,23 @@
+const a =document.getElementById("hello");
+
+a.textContent ="Welcome to our website!";
+
+
+
+const btn = document.getElementById('one');
+function change(){
+    btn.textContent ="clicked"
+    document.body.style.background="#f393f3"
+}
+
+
+
+const img = document.getElementById('image1');
+img.style.height ="200px"
+const imgbtn = document.getElementById('imgbtn');
+
+imgbtn.addEventListener("click", () => {
+    const isHidden = img.style.visibility === "hidden";
+    img.style.visibility = isHidden ? "visible" : "hidden";
+    imgbtn.textContent =isHidden ? "visible" : "hidden";
+});
