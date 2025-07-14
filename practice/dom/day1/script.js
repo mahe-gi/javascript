b7cadf6b5ddfca0889a370af2659651a85683abf
mahesh ch <chmahesh.dev@gmail.com>
2025-07-14 16:37:29 +0530

    dom

--- practice/dom/day1/script.js
+++ practice/dom/day1/script.js
@@ -1,23 +1,19 @@
-const a =document.getElementById("hello");
+const a = document.getElementById("hello");
 
-a.textContent ="Welcome to our website!";
+a.textContent = "Welcome to our website!";
 
-
-
-const btn = document.getElementById('one');
-function change(){
-    btn.textContent ="clicked"
-    document.body.style.background="#f393f3"
+const btn = document.getElementById("one");
+function change() {
+  btn.textContent = "clicked";
+  document.body.style.background = "#f393f3";
 }
 
-
-
-const img = document.getElementById('image1');
-img.style.height ="200px"
-const imgbtn = document.getElementById('imgbtn');
+const img = document.getElementById("image1");
+img.style.height = "200px";
+const imgbtn = document.getElementById("imgbtn");
 
 imgbtn.addEventListener("click", () => {
-    const isHidden = img.style.visibility === "hidden";
-    img.style.visibility = isHidden ? "visible" : "hidden";
-    imgbtn.textContent =isHidden ? "visible" : "hidden";
+  const isHidden = img.style.visibility === "hidden";
+  img.style.visibility = isHidden ? "visible" : "hidden";
+  imgbtn.textContent = isHidden ? "visible" : "hidden";
 });
